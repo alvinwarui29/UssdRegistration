367956b929406fe8b3db5cdd2aa25fbd7f2c72d6
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-14 02:51:34 +0300

    add user model class

--- connection.php
+++ connection.php
@@ -1,13 +1,38 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = "";
-$dbname = 'ussdregistration';
 
-$dsn = 'mysql:host='. $host . ';dbname =' .$dbname;
-$pdo = new PDO($dsn,$user,$password);
-$pdo ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
-PDO::FETCH_ASSOC);
+class DBConnector{
+    var $pdo;
+    function __construct(){
+        $host = 'localhost';
+        $user = 'root';
+        $password = "";
+        $dbname = 'ussdregistration';
+        $dsn = 'mysql:host='. $host . ';dbname =' .$dbname;
+        
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC
+        ];
+        
+        try{
+            $this->pdo = new PDO($dsn,$user,$password);
+            
+        echo "Success";
+
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+       
+    }
+    public function connectToDB(){
+        return $this->pdo;
+    }
+    public function closeDB(){
+        $this->pdo = null;
+    }
+}
 
 
 ?>
--- user.php
+++ user.php
@@ -1,6 +1,29 @@
 <?php
 class User {
-    
+    protected $id;
+    protected $location;
+
+function __construct($id){
+    $this->id = $id;
+}
+public function getId(){
+    return $this->id;
+}
+public function setLocation($location){
+    $this->id = $location;
+}
+public function getLocation(){
+    return $this->location;
+}
+public function setId($id){
+    $this->id = $id;}
+
+public function register($pdo){
+    $stmt = $pdo->prepare("INSERT INTO ussdregistration.relief_requests
+    (id_number,location) values(?,?)");
+    $stmt-> execute([$this->getId(),$this->getLocation()]);
+
+}
 }
 
 ?>

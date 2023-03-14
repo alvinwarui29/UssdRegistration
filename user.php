ce031eb84c33191bc8eebb59724a27a9f557b744
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-14 03:06:55 +0300

    add check for if user is registered and register functionality

--- user.php
+++ user.php
@@ -19,10 +19,22 @@ public function setId($id){
     $this->id = $id;}
 
 public function register($pdo){
+    try{
     $stmt = $pdo->prepare("INSERT INTO ussdregistration.relief_requests
     (id_number,location) values(?,?)");
     $stmt-> execute([$this->getId(),$this->getLocation()]);
-
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+}
+public function isUserRegistered ($pdo){
+    $stmt = $pdo->prepare("SELECT * FROM ussdsms.user WHERE id_number=?");
+    $stmt->execute([$this->getId()]);
+    if(count($stmt->fetchAll())>0){
+        return true;
+    }else{
+        return false;
+    }
 }
 }
 
